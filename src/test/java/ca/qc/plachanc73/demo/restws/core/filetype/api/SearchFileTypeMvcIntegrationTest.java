605fc849d28b82c0dce0abe3c9f95f9c439a8ce5
Pascal Lachance <plachance@spscommerce.com>
2025-02-13 10:37:41 -0500

    Upgrade MockBean to MockitoBean

--- src/test/java/ca/qc/plachanc73/demo/restws/core/filetype/api/SearchFileTypeMvcIntegrationTest.java
+++ src/test/java/ca/qc/plachanc73/demo/restws/core/filetype/api/SearchFileTypeMvcIntegrationTest.java
@@ -10,9 +10,9 @@ import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -36,7 +36,7 @@ class SearchFileTypeMvcIntegrationTest {
     @Autowired
     MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     FileTypeSearchService fileTypeSearchService;
 
     @BeforeAll
